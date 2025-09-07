a9dc7c755fd0fa253155f676c53fdb1c1465e6a6
Pamela Salas <pamelasalas@Pamelas-MacBook-Air.local>
2025-09-07 17:02:26 -0500

    first commit

---
+++ wdv341.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homework Page</title>
+</head>
+<body>
+    <a href="wdv341.php">WDV341 Homework Page</a>
+    <h1>test</h1>
+    
+</body>
+</html>
